a31a0ccbf8969cbd38661c63fc35750c5a8af4a6
Alexandra DeWit <alexa.dewit@gmail.com>
2018-07-22 17:49:57 +0200

    Instance tests

--- test/Instances.purs
+++ test/Instances.purs
@@ -12,4 +12,16 @@ import Crypt.NaCl
 runInstanceTests :: Effect Unit
 runInstanceTests = do
   log "Running typeclass instance tests"
+  keyA     <- generateSecretBoxKey
+  keyB     <- generateSecretBoxKey
+  let differentKeys = keyA == keyB
+  let sameKey = keyA == keyA
+  assert sameKey
+  assert $ not differentKeys
+  aliceKp <- generateBoxKeyPair
+  bobKp   <- generateBoxKeyPair
+  let differentKeyPairs = aliceKp == bobKp
+  let sameKeyPair = aliceKp == aliceKp
+  assert sameKeyPair
+  assert $ not differentKeyPairs
 
